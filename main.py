eb97c336579c6eb07a5c2c485345e4a4e925aa3a
Luke Tainton <luke@tainton.uk>
2020-02-08 14:54:04 +0000

    Lint: E501
    Bring line under 127 characters.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from datetime import datetime
 def parse_args():
     parser = argparse.ArgumentParser(description="This script gets IP address information.")
     parser.add_argument("ip", help="IP Address. Specify 'me' or an IP address.")
-    parser.add_argument("-p", "--prefixes", dest='prefixes', action='store_true', help="Get IPv4 prefixes advertised by identified AS.")
+    parser.add_argument("-p", "--prefixes", dest='prefixes', action='store_true', help="Get IPv4 prefixes advertised by AS.")
     parser.add_argument("-o", "--output", dest='output', action='store_true', help="Write output of script to a file.")
     return parser.parse_args()
 
